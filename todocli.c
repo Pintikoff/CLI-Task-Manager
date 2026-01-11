66fe85bec772ccec02d2aab62cc6f1bf709cbd77
Pintikoff <petrovskiy.lev@gmail.com>
2026-01-11 23:45:06 +0100

    Added an updateFile fuction. function goes through a struct and writes every value into a file. addTodo, ListTodo dont overwrite todocli.txt file directly. Added a deleteTodo function. added a clearTodo function. + Bug fixes

--- todocli.c
+++ todocli.c
@@ -8,13 +8,8 @@ struct Todo
     char content[30];
 };
 
-void helpCmd(){
-    printf("THIS  IS  A  HELP  PAGE  FOR  todocli.\n\n");
-    printf(" -a [id]: Add command adds a given string to a todo list.\n");
-    printf(" -l : Lists the todolist.\n");
-}
-
-int readJSON(struct Todo **todoList){
+//adds todos from a file to a struct
+int readTodo(struct Todo **todoList){
     FILE *ftpr;
     ftpr = fopen("todocli.txt","r");
 
@@ -37,42 +32,29 @@ int readJSON(struct Todo **todoList){
     int i = 0;
     while (fgets(lineContent, sizeof(lineContent), ftpr))
     {
-        int id;
+        int trash;
         char content[30];
 
-        sscanf(lineContent, "%d, %[^;]", &id, content);
-        (*todoList)[i].id = id;
+        sscanf(lineContent, "%d: %s", &trash, content);
+        (*todoList)[i].id = i+1;
         strcpy((*todoList)[i].content, content);
-
         i++;
     }
 
     return count;
 }
 
-void addTodo(struct Todo *todoList, char *content, int count){
+void updateFile(struct Todo *todoList, int count){
+    FILE *delete;
+    delete = fopen("todocli.txt", "w");
+
     FILE *ftpr;
     ftpr = fopen("todocli.txt","a");
-
-    todoList[count].id = count+1;
-    strcpy(todoList[count].content, content);
-
-    printf("Task added succesfully (ID: %d): %s\n", todoList[count].id, todoList[count].content);
-
-    fprintf(ftpr, "%d: %s \n", todoList[count].id, todoList[count].content);
-    fclose(ftpr);
-}
-
-void printTodo(){
-    FILE *ftpr;
-    ftpr = fopen("todocli.txt","r");
-
-    char fileContent[100];
-    while (fgets(fileContent, 100, ftpr))
-    {
-        printf("%s", fileContent);
+    fprintf(delete, "");
+    for(int i = 0; i < count; i++){
+        fprintf(ftpr, "%d: %s \n", todoList[i].id, todoList[i].content);
     }
-
+    fclose(ftpr);
 }
 
 void genASCII(){
@@ -85,9 +67,56 @@ void genASCII(){
     printf("            \\/__/     \\/__/     \\/__/          \\/__/                      \n");
 }
 
+void addTodo(struct Todo *todoList, char *content, int count){
+    todoList[count].id = count+1;
+    strcpy(todoList[count].content, content);
+
+    printf("Task added succesfully (ID: %d): %s\n", todoList[count].id, todoList[count].content);
+
+    updateFile(todoList, count+1);
+}
+
+void listTodo(struct Todo *todoList,int count){
+    for(int i = 0; i < count; i++){
+        printf("%d: %s.\n", todoList[i].id, todoList[i].content);
+    }
+}
+
+void deleteTodo(struct Todo *todoList, char *argv, int *count){
+    int givenId = atoi(argv);
+    if (givenId <= 0 || givenId > *count){
+        return;
+    }
+    int wantedInd = givenId - 1;
+    for(int i = wantedInd; i < *count; i++){
+        todoList[i] = todoList[i+1];
+        todoList[i-1].id = i;
+    }
+    
+    (*count) --;
+    updateFile(todoList, *count);
+}
+
+void clearTodo(){
+    FILE *ftpr;
+    ftpr = fopen("todocli.txt","w");
+    fprintf(ftpr, "");
+}
+
+void helpTodo(){
+    printf("\n");
+    printf("THIS  IS  A  HELP  PAGE  FOR  todocli.\n\n");
+    printf("   -a   : Add command adds a given string to a todo list.\n");
+    printf("   -l   : Lists the todolist.\n");
+    printf(" -d [id]: Deletes a chosen task.\n");
+    printf(" -u [id]: Update a chosen task.\n");
+    printf(" -clear : deletes WHOLE todo-todo list (clears whole data).\n");
+    printf("   -h   : Displays this help command.\n\n");
+}
+
 int main(int argc, char* argv[]){
     struct Todo *todoList = NULL;
-    int count = readJSON(&todoList);
+    int count = readTodo(&todoList);
     if (todoList == NULL){
         printf("error");
         return 1;
@@ -96,18 +125,28 @@ int main(int argc, char* argv[]){
     //genASCII();
     if(argc >= 2){
         if(strcmp(argv[1], "-a") == 0 && argv[2]){
-                addTodo(todoList, argv[2], count);  
+            addTodo(todoList, argv[2], count);  
         }
         else if (strcmp(argv[1], "-l") == 0){
-            printTodo();
+            listTodo(todoList,count);
+        }
+        else if (strcmp(argv[1], "-d") == 0 && argv[2]){
+            deleteTodo(todoList, argv[2], &count);
+        }
+        else if (strcmp(argv[1], "-clear") == 0){
+            clearTodo();
         }
         else if (strcmp(argv[1], "-h") == 0){
             genASCII();
-            helpCmd();
+            helpTodo();
         }
         else{
             printf("No such option is available. Check the -h command");
         }
+    }
+    else{
+        genASCII();
+        helpTodo();
     }
     free(todoList);
     return 0;
